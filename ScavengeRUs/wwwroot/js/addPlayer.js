d3cd9a57d5d12ec7da3e2308afb52aa624ce6cb5
EvasiveAce <hensley.ethan64@gmail.com>
2023-11-27 16:41:11 -0500

    Added a sort feature to the Add Players to hunt button.

---
+++ ScavengeRUs/wwwroot/js/addPlayer.js
@@ -0,0 +1,62 @@
+﻿'use strict';
+
+// sorting hunts by status in the hunt display page/index.cshtml
+let allButton = document.getElementById("All");
+let activeButton = document.getElementById("Add");
+let pendingButton = document.getElementById("Remove");
+
+
+var tableRows = document.getElementsByTagName("tr");
+
+allButton.addEventListener("click", (e) => {
+    showHunts();
+});
+
+activeButton.addEventListener("click", (e) => {
+    showHunts();
+    hideEndedHunts();
+});
+
+pendingButton.addEventListener("click", (e) => {
+    showHunts();
+    hideActiveHunts();
+});
+
+
+function hideHunts() {
+    huntTable.style.visibility = "hidden";
+}
+
+function showHunts() {
+    for (var i = 0; i < tableRows.length; i++) {
+
+        tableRows[i].style.display = "";
+    }
+};
+
+function hideEndedHunts() {
+    for (var i = 0; i < tableRows.length; i++) {
+
+        var cells = tableRows[i].getElementsByTagName("td");
+
+        for (var j = 0; j < cells.length; j++) {
+
+            if (cells[j].innerHTML.includes("Remove", i)) {
+                tableRows[i].style.display = "none";
+            }
+        }
+    }
+};
+function hideActiveHunts() {
+    for (var i = 0; i < tableRows.length; i++) {
+
+        var cells = tableRows[i].getElementsByTagName("td");
+
+        for (var j = 0; j < cells.length; j++) {
+
+            if (cells[j].innerHTML.includes("Add", i)) {
+                tableRows[i].style.display = "none";
+            }
+        }
+    }
+};
